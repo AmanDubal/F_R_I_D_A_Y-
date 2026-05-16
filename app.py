19f288435fb67108b9772a972837770c68a676f1
AmanDubal <143494268+AmanDubal@users.noreply.github.com>
2026-05-16 10:38:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -20,28 +17,33 @@ def home():
 
 @app.route("/chat", methods=["POST"])
 def chat():
+
     try:
+
         data = request.get_json()
         user_message = data.get("message")
 
         response = client.chat.completions.create(
             model="openai/gpt-4o-mini",
             messages=[
-                {"role": "user", "content": user_message}
-            ],
-            max_tokens=300
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ]
         )
 
-        ai_response = response.choices[0].message.content
-
         return jsonify({
-            "response": ai_response
+            "response": response.choices[0].message.content
         })
 
     except Exception as e:
+
         return jsonify({
             "error": str(e)
         }), 500
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
